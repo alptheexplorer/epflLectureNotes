1a1bee67e0d7b2e2dfcc9dfa6bfdbad0509713d9
alptheexplorer <alpozen@gmail.com>
2019-11-01 02:42:14 +0100

    pdf update

--- compilePDF.sh
+++ compilePDF.sh
@@ -8,6 +8,11 @@ git pull origin
 
 pdflatex -interaction=nonstopmode -aux-directory=<auxFiles> -output-directory=<analysis> analysis/analysis.tex
 
+pdflatex -interaction=nonstopmode -aux-directory=<auxFiles> -output-directory=<analysis> advancedComputation/aicc.tex
+
+pdflatex -interaction=nonstopmode -aux-directory=<auxFiles> -output-directory=<analysis> linearAlgerba/linearAlgebra.tex
+
+
 git add .
 git commit -m "pdf update"
 git push origin 
