36e88c4da105b1992c69d514782c1050cbe20721
alptheexplorer <alpozen@gmail.com>
2019-11-01 02:55:24 +0100

    pdf update

--- compilePDF.sh
+++ compilePDF.sh
@@ -6,11 +6,9 @@ cd epflLectureNotes/epflLectureNotes
 
 git pull origin
 
-pdflatex -interaction=nonstopmode -aux-directory=auxFiles -output-directory=analysis analysis/analysis.tex
+pdflatex -interaction=nonstopmode analysis/analysis.tex
 
-pdflatex -interaction=nonstopmode -aux-directory=<auxFiles> -output-directory=<analysis> advancedComputation/aicc.tex
 
-pdflatex -interaction=nonstopmode -aux-directory=<auxFiles> -output-directory=<analysis> linearAlgerba/linearAlgebra.tex
 
 
 git add .
